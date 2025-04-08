7acd78654ee7640d8d98cb747a75712698895ff1
nicolamenace <nicolasmadec999@gmail.com>
2025-04-08 13:36:21 +0200

    Finalisation de la section première année

--- src/routes/premannee/projects/+layout.ts
+++ src/routes/premannee/projects/+layout.ts
@@ -11,9 +11,8 @@ export const load = async () => {
         { title: "Morpion", image: "/images/premannee/morpion.jpg", link: `/premannee/projects/${normalizeSlug("Morpion")}`, description: "Description for Morpion", content: "Detailed content about Morpion goes here." },
         { title: "Jeu de Nim", image: "/images/premannee/jeu_de_nim.jpg", link: `/premannee/projects/${normalizeSlug("Jeu de Nim")}`, description: "Description for Jeu de Nim", content: "Detailed content about Jeu de Nim goes here." },
         { title: "Horloge", image: "/images/premannee/horloge.png", link: `/premannee/projects/${normalizeSlug("Horloge")}`, description: "Description for Horloge", content: "Detailed content about Horloge goes here." },
-        { title: "Bdd Agricole", image: "/images/premannee/projet4.jpg", link: `/premannee/projects/${normalizeSlug("Bdd Agricole")}`, description: "Description for Projet 4", content: "Detailed content about Projet 4 goes here." },
-        { title: "Projet 5", image: "/images/premannee/projet5.jpg", link: `/premannee/projects/${normalizeSlug("Projet 5")}`, description: "Description for Projet 5", content: "Detailed content about Projet 5 goes here." },
-        { title: "Projet 6", image: "/images/premannee/projet6.jpg", link: `/premannee/projects/${normalizeSlug("Projet 6")}`, description: "Description for Projet 6", content: "Detailed content about Projet 6 goes here." }
+        { title: "Bdd Agricole", image: "/images/premannee/bdd_cereals.png", link: `/premannee/projects/${normalizeSlug("Bdd Agricole")}`, description: "Description for Projet 4", content: "Detailed content about Projet 4 goes here." },
+        { title: "Sites Web", image: "/images/premannee/siteweb.jpg", link: `/premannee/projects/${normalizeSlug("Sites Web")}`, description: "Description for Projet 5", content: "Detailed content about Projet 5 goes here." }
     ];
 
     return { projects }; // Ensure projects is returned
